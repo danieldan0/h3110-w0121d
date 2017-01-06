4fda68b097fe1ae6ecc33248a771d804de783866
nope <1337h4x0rname@gmail.com>
2017-01-05 19:02:19 -0500

    another commit
    blah

--- Simulation.cpp
+++ Simulation.cpp
@@ -1,5 +1,27 @@
+#include <cmath>
+#include <stdio.h>
 #include <iostream>
+#include <graphics.cpp>    //will create this file soon :D
 #define XRES 100
 #define YRES 100
 
 int parts[XRES][YRES];
+
+int main()    //just put this here because main() is always needed in cpp
+{
+};
+
+struct particle
+{
+  int vx;
+  int vy;
+  int type;
+  int ctype;
+  float temp;
+  int tmp;
+  int tmp2;
+}
+
+class Simulation
+{
+}
